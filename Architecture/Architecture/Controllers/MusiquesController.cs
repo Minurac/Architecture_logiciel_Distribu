644c32a273a4b22ab5277924815d8948f1e4d721
Hugo Brangbour <strappeur0007@gmail.com>
2020-12-01 12:00:45 +0100

    Init3

--- Architecture/Architecture/Controllers/MusiquesController.cs
+++ Architecture/Architecture/Controllers/MusiquesController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Castle.Core.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyTunes.Models;

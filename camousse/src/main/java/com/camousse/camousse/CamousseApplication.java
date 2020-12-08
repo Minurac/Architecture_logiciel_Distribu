2144b582417da094da4ee9259367e169bda010d7
Théo Mielnikoff <theo.mielnikoff@gmail.com>
2020-12-08 11:36:33 +0100

    init

---
+++ camousse/src/main/java/com/camousse/camousse/CamousseApplication.java
@@ -0,0 +1,13 @@
+package com.camousse.camousse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamousseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamousseApplication.class, args);
+	}
+
+}
---
+++ camousse/src/test/java/com/camousse/camousse/CamousseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camousse.camousse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamousseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

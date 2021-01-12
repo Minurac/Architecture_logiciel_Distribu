c2bae3ae9e336e3a661bff2bb104632022ade075
Théo Mielnikoff <theo.mielnikoff@gmail.com>
2021-01-12 18:36:42 +0100

    final

--- camousse_back/src/main/java/com/camousse/camousse/controller/ProduitController.java
+++ camousse_back/src/main/java/com/camousse/camousse/controller/ProduitController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.camousse.camousse.bo.Nomenclature;
 import com.camousse.camousse.bo.Produit;
 import com.camousse.camousse.repository.ProduitRepository;
 
--- camousse_back/src/main/java/com/camousse/camousse/controller/UtilisateurController.java
+++ camousse_back/src/main/java/com/camousse/camousse/controller/UtilisateurController.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.camousse.camousse.bo.Profil;
-import com.camousse.camousse.bo.Taxe;
 import com.camousse.camousse.bo.Utilisateur;
 import com.camousse.camousse.repository.UtilisateurRepository;
 
